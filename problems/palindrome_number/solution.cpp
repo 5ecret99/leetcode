b1ebc02eb90c4f4f5ef5fcfddd74f617f1cec080
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2023-04-16 19:29:27 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

---
+++ problems/palindrome_number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+
+        long reverse_x = 0;
+        int temp_x = x;
+        while(temp_x){
+            reverse_x = 10 * reverse_x + temp_x % 10;
+            temp_x/= 10;
+        }
+
+        return reverse_x == x;
+    }
+};
