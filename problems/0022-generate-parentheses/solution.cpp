f858d05689f31d86d537a47dd6721e261868b8f7
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2023-05-20 20:32:52 +0000

    [LeetCode Sync] Runtime - 4 ms (45.36%), Memory - 13.8 MB (68.22%)

---
+++ problems/0022-generate-parentheses/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> res;
+
+    void helper(int open, int close, int n, string current){
+        if(current.size() == n * 2){
+            res.push_back(current);
+        }
+
+        if(open < n){
+            helper(open + 1, close, n, current + "(");
+        }
+
+        if(close < open){
+            helper(open, close + 1, n, current + ")");
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        helper(0, 0, n, "");
+        return res;
+    }
+};
