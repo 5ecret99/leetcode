75d8b32ab96c5b56c7880aafdfd38ec491f1c640
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2023-07-25 19:19:11 +0000

    [LeetCode Sync] Runtime - 3 ms (70.89%), Memory - 8.3 MB (100.00%)

---
+++ problems/0338-counting-bits/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n){
+            n &= (n - 1);
+            res++;
+        }
+
+        return res;
+    }
+public:
+    vector<int> countBits(int n) {
+        vector<int> res; 
+        for(int i = 0; i <= n; i++)
+            res.push_back(hammingWeight(i));
+
+        return res;
+    }
+
+    
+};
