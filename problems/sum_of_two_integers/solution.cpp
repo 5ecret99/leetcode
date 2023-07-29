4128f1b8b5970249ec63c4f40f6179a31b38e11c
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2023-07-29 21:09:43 +0000

    Sync LeetCode submission - Sum of Two Integers (cpp)

--- problems/sum_of_two_integers/solution.cpp
+++ problems/sum_of_two_integers/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int getSum(int a, int b) {
+    int getSum(unsigned int a, unsigned int b) {
         if(!b) return a;
-        return getSum(a ^ b, (unsigned int)(a & b) << 1);
+        return getSum(a ^ b, (a & b) << 1);
     }
 };
