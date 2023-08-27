728677c6276898f6362d6531f6122e28547c887e
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2023-08-27 20:01:45 +0000

    Sync LeetCode submission - K Closest Points to Origin (cpp)

---
+++ problems/k_closest_points_to_origin/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    using point = vector<int>;
+    struct{
+        bool operator() (const point& pa, const point& pb){
+            return pa[0] * pa[0] - pb[0] * pb[0] + pa[1] * pa[1] - pb[1] * pb[1] < 0;
+        }
+    }compare_points;
+
+public:
+    vector<point> kClosest(vector<point>& points, int k) {
+        sort(points.begin(), points.end(), compare_points);
+        return vector<point>{points.begin(), points.begin() + k};
+    }
+};
+
