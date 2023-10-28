488378f05004e07414355a0d1fa1c6c6fa1e21ff
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2023-10-28 17:53:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (100.00%)

---
+++ problems/0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int ind = m + n - 1;
+        while(i2 >= 0)
+            nums1[ind--] = i1 >= 0 && nums1[i1] > nums2[i2] ? nums1[i1--] 
+                                                            : nums2[i2--]; 
+    }
+};
