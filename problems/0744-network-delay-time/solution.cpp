ca7c3bfe41557b4b65ddfbb03fee29d3108d6476
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-04-01 18:24:13 +0000

    [LeetCode Sync] Runtime - 75 ms (84.52%), Memory - 43.2 MB (93.82%)

--- problems/0744-network-delay-time/solution.cpp
+++ problems/0744-network-delay-time/solution.cpp
@@ -1,37 +1,34 @@
 class Solution {
-private:
-    struct pair_comp{
-        bool operator()(const pair<int, int>& a, const pair<int, int>& b){
-            return a.first > b.first; 
-        }
-    };
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<pair<int, int>>> mp;
-        for(int i = 0; i < times.size(); ++i)
-            mp[times[i][0]].emplace_back(times[i][1], times[i][2]);
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
+    {
+        using edge = pair<int, int>; //(weight, node)
 
-        vector<int> distances(n + 1, INT_MAX);
-        distances[k] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, pair_comp> min_heap;
-        min_heap.emplace(0,k);
+        vector<edge> adj[n + 1];
+        for(const auto& t : times)
+            adj[t[0]].emplace_back(t[2], t[1]);
+        
+        priority_queue<edge, vector<edge>, greater<edge>> pq;
+        pq.emplace(0, k);
 
-        while(!min_heap.empty()){
-            auto [weight, node] = min_heap.top();
-            min_heap.pop();
-            for(auto [nnode, nweight] : mp[node])
-                if(weight + nweight < distances[nnode]){
-                    distances[nnode] = weight + nweight;
-                    min_heap.emplace(distances[nnode], nnode);
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+
+        while(!pq.empty())
+        {
+            auto [time, node] = pq.top();
+            pq.pop();
+
+            for(const auto& [ctime, cnode] : adj[node])
+                if(time + ctime < dist[cnode])
+                {
+                    dist[cnode] = time + ctime;
+                    pq.emplace(dist[cnode], cnode);
                 }
         }
 
-        int max_dist{0};
-        for(auto it = distances.begin() + 1; it != distances.end(); ++it){
-            if(*it == INT_MAX) return -1;
-            max_dist = max(max_dist, *it);
-        }
-        
-        return max_dist;
+        auto mx = *max_element(dist.begin() + 1, dist.end());
+
+        return mx < INT_MAX ? mx : -1;
     }
 };
