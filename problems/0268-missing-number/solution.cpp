4fddabfc4f771a73e47f1b6cf2c0a1d866ea0bdc
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-05-03 18:59:29 +0000

    [LeetCode Sync] Runtime - 3 ms (99.87%), Memory - 20.3 MB (57.74%)

--- problems/0268-missing-number/solution.cpp
+++ problems/0268-missing-number/solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-    int length=nums.size();
-    int asum=0;
-    for(int i=0;i<length;i++) asum+=nums[i];
-    
-    return (length+1)*length/2-asum;     
+    int missingNumber(vector<int>& nums) 
+    {
+        unsigned long long res = nums.size();
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            res^= i;
+            res^= nums[i];
+        }
+
+        return res;
     }
 };
