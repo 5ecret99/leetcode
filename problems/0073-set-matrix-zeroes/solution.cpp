55d8ff3ba47a10be4ba277c820fc8c4e81c4d44b
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-10-27 21:35:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.4 MB (9.86%)

--- problems/0073-set-matrix-zeroes/solution.cpp
+++ problems/0073-set-matrix-zeroes/solution.cpp
@@ -5,14 +5,15 @@ public:
         unordered_set<int> column;
         for(int i = 0; i < matrix.size(); i++) 
             for(int j = 0; j < matrix[0].size(); j++) 
-                if(!matrix[i][j]){
+                if(!matrix[i][j])
+                {
                     row.insert(i);
                     column.insert(j);
                 }
         
         for(int i = 0; i < matrix.size(); i++) 
             for(int j = 0; j < matrix[0].size(); j++) 
-                if(row.find(i) != row.end() || column.find(j) != column.end())
+                if(row.contains(i) || column.contains(j))
                     matrix[i][j] = 0;
     }
 };
