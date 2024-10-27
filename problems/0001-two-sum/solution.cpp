0b00eedb1edbff6377b37c4b6ccaf95a0349673f
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-10-27 15:09:55 +0000

    [LeetCode Sync] Runtime - 0 ms (99.99%), Memory - 14.4 MB (36.11%)

--- problems/0001-two-sum/solution.cpp
+++ problems/0001-two-sum/solution.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-        vector<int> res;
-        for(int i = 0; i < nums.size(); i++){
-            auto it = map.find(nums[i]);
-            if(it != map.end()){
-                res.push_back(it->second);
-                res.push_back(i);
-                map.erase(it);
-            }
-            else{
-                map.insert({target - nums[i], i});
-            }
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(mp.count(nums[i]))
+                return {i, mp[nums[i]]};
+            
+            mp[target - nums[i]] = i;
         }
 
-        return res;
+        return {};
     }
 };
