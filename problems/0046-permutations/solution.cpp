09d0e1667dc55015a05f2f73666c2d7ac6282b35
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-10-28 15:01:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (55.74%)

--- problems/0046-permutations/solution.cpp
+++ problems/0046-permutations/solution.cpp
@@ -1,21 +1,27 @@
-class Solution {
+class Solution 
+{
 private:
-    vector<vector<int>> res{};
-    void helper(vector<int>& nums, vector<int>::iterator it){
-        if(it == nums.end()){
+    vector<vector<int>> res;
+    void permute(const vector<int>& nums, vector<int>::iterator it)
+    {
+        if(it == nums.end())
+        {
             res.push_back(nums);
             return;
         }
-
-        for(auto pt{it}; pt != nums.end(); ++pt){
+        
+        for(auto pt = it; pt < nums.end(); ++pt)
+        {
             swap(*it, *pt);
-            helper(nums, it + 1);
+            permute(nums, it + 1);
             swap(*it, *pt);
         }
     }
+
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        helper(nums, nums.begin());
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        permute(nums, nums.begin()); 
         return res;
     }
 };
