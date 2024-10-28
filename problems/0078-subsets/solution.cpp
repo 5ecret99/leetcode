665954fb6f56b15bbd7294c1c760cd896a347c5d
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-10-28 14:21:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (74.52%)

--- problems/0078-subsets/solution.cpp
+++ problems/0078-subsets/solution.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs{{}};
-        for(const auto num : nums){
-            size_t n = subs.size();
-            for(size_t i = 0; i < n; ++i){
-                subs.push_back(subs[i]);
-                subs.back().push_back(num);
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<vector<int>> res;
+        res.push_back({});
+        for(auto num : nums)
+        {
+            int n = res.size();
+            for(int i = 0; i < n; ++i)
+            {
+                res.push_back(res[i]);
+                res.back().push_back(num);
             }
-        }
-       
-       return subs;
+        } 
+        
+        return res;
     }
 };
