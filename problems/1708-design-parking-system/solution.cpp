ca1ca24e16e4e25d4eb2923ffd250f93980c8275
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-11-06 22:13:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 37 MB (81.38%)

---
+++ problems/1708-design-parking-system/solution.cpp
@@ -0,0 +1,24 @@
+class ParkingSystem 
+{
+private:
+    array<int, 3> park;
+public:
+
+    ParkingSystem(int big, int medium, int small)
+    {
+        park[0] = big;
+        park[1] = medium;
+        park[2] = small;    
+    }
+    
+    bool addCar(int carType) 
+    {
+        return --park[carType - 1] >= 0;    
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
