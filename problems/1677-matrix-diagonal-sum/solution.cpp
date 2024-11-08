2f80e674e5a698a81043ace25d5acafb1b947a44
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-11-08 19:43:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14 MB (79.49%)

---
+++ problems/1677-matrix-diagonal-sum/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) 
+    {
+        int n = mat.size();
+        int res = 0;
+        for(int i = 0; i < n; ++i)
+            res+= mat[i][i] + mat[i][n -i - 1];
+        
+        return res - (n & 1 ? mat[n / 2][n / 2] : 0 );
+    }
+};
