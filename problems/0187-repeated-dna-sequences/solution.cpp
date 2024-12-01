886d1fc63a82203fa6fd73bd9cc851bc1d0b5288
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2024-12-01 21:19:42 +0000

    [LeetCode Sync] Runtime - 30 ms (96.49%), Memory - 21.9 MB (88.44%)

--- problems/0187-repeated-dna-sequences/solution.cpp
+++ problems/0187-repeated-dna-sequences/solution.cpp
@@ -3,17 +3,17 @@ class Solution
 private:
     static constexpr int SEQ_SIZE = 10;
 public:
-    vector<string> findRepeatedDnaSequences(string s)
+    vector<string> findRepeatedDnaSequences(string_view s)
     {
         int n = s.size();
-        unordered_map<string, int> DNA;
+        unordered_map<string_view, int> DNA;
         for(int i = 0; i <= n - SEQ_SIZE; ++i)
             DNA[s.substr(i, SEQ_SIZE)]++;
         
         vector<string> res;
-        for(auto& it : DNA)
-            if(it.second > 1)
-                res.push_back(it.first);
+        for(const auto& [seq, cnt] : DNA)
+            if(cnt > 1)
+                res.push_back(string(seq));
 
        return res;
     }
