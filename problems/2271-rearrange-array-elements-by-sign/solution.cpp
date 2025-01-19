693cb7e3c472657a97cfcca13edec4dbf8f627bf
5ecret99 <79488663+5ecret99@users.noreply.github.com>
2025-01-19 22:31:15 +0000

    [LeetCode Sync] Runtime - 4 ms (80.77%), Memory - 128.4 MB (59.17%)

---
+++ problems/2271-rearrange-array-elements-by-sign/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        int pos = 0;
+        int neg = 1;
+        vector<int> res(nums.size());
+
+        for(auto num : nums)
+        {
+            if(num > 0)
+            {
+                res[pos] = num;
+                pos+= 2;
+            }
+            else
+            {
+                res[neg] = num;
+                neg+= 2;
+            }
+        }
+         
+        return res;
+    }
+};
